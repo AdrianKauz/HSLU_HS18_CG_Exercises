b1e6e27459c884c7ea4dd38ba7064c11c54caff9
Adrian Kauz <akauz@hispeed.ch>
2018-11-25 22:13:38 +0100

    - Start with preparation final Exercise Part 6

--- WebGLPart5_Exercise3ext/shaders/FragmentShader.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-varying highp vec2 vTextureCoord;
-varying lowp vec4 vColor;
-
-uniform sampler2D uSampler;
-
-void main() {
-    gl_FragColor = texture2D(uSampler, vTextureCoord) + vColor;
-}
